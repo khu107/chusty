234771f67fa719e93826e33ac3f0b1fd9d5eab57
metablog <khu10735@naver.com>
2024-02-25 01:18:00 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,2 @@
-import moment from "moment";
-
-const currentTime = moment().format("YYYY MM DD");
-
-console.log(currentTime);
-
-const person: string = "David";
-const count: number = 100;
+import dotenv from "dotenv";
+dotenv.config();
