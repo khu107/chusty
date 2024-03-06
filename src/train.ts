80cc78c08f1c04fd9650177f6f2da988d942154b
metablog <khu10735@naver.com>
2024-03-06 15:37:04 +0900

    feat: Task - I

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,12 @@
+// Task - I
+
+function getDigits(input: string): string {
+  const result = input.match(/\d+/g)?.join("");
+  return result ? result : "";
+}
+
+console.log(getDigits("m14i1t"));
+
 /* Project Standards:
   - Logging standards
   - Naming standards
