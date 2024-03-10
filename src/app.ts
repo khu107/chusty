0587572183827bdf397089e80fa9c5abb8533aaf
metablog <khu10735@naver.com>
2024-03-11 01:24:12 +0900

    feat: integrate middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,34 @@ import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
 
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+  uri: String(process.env.MONGO_URL),
+  collection: "session",
+});
+
 // 1 - ENTRANCE
 const app: Application = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
+
 // 2 - SESSIONS
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000 * 3600 * 3, // 3h
+    },
+    store: store,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 // 3 - VIEWS
 app.set("views", path.join(__dirname, "views"));
