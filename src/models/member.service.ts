5f3ffa8c2770ac1179e4acdc6b2ce7c2a506dce3
metablog <khu10735@naver.com>
2024-03-23 00:46:14 +0900

    fix: modify spa login api process

--- src/models/member.service.ts
+++ src/models/member.service.ts
@@ -7,7 +7,7 @@ import {
   MemberUpdateInput,
 } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Error";
-import { MemberType } from "../libs/enums/member.enum";
+import { MemberStatus, MemberType } from "../libs/enums/member.enum";
 import * as bcrypt from "bcryptjs";
 class MemberService {
   private readonly memberModel;
@@ -33,12 +33,17 @@ class MemberService {
     // TODO: Consider member status later
     const member = await this.memberModel
       .findOne(
-        { memberNick: input.memberNick },
-        { memberNick: 1, memberPassword: 1 }
+        {
+          memberNick: input.memberNick,
+          memberStatus: { $ne: MemberStatus.DELETE },
+        },
+        { memberNick: 1, memberPassword: 1, memberStatus: 1 }
       )
       .exec();
     if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
-
+    else if (member.memberStatus === MemberStatus.BLOCK) {
+      throw new Errors(HttpCode.FORBIDDEN, Message.BLOCKED_USER);
+    }
     const isMatch = await bcrypt.compare(
       input.memberPassword,
       member.memberPassword
