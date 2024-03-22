ea1a09cd649d8ffc6b10e6db97a605337a093e28
metablog <khu10735@naver.com>
2024-03-23 00:30:01 +0900

    feat: frontend chosen user update logic develop

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -1,1 +1,28 @@
 console.log("Users frontend javascript file");
+
+$(function () {
+  $(".member-status").on("change", (e) => {
+    const id = e.target.id;
+    console.log(id);
+
+    const memberStatus = $(`#${id}.member-status`).val();
+    console.log(memberStatus);
+
+    axios
+      .post("/admin/user/edit", { _id: id, memberStatus: memberStatus })
+      .then((response) => {
+        console.log("response", response);
+        const result = response.data;
+        console.log("result", result);
+
+        if (result.data) {
+          console.log("User update!");
+          $(".member-status").blur();
+        } else alert("User update failed!");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("User update failed!");
+      });
+  });
+});
