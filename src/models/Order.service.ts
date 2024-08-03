288f2bb9db4582245de55ad8e2fcb2e8293910c2
metablog <khu10735@naver.com>
2024-08-04 04:50:49 +0900

    fix: test

--- src/models/Order.service.ts
+++ src/models/Order.service.ts
@@ -10,11 +10,7 @@ import {
 import { shapeIntoMongooseObjectId } from "../libs/config";
 import Errors, { HttpCode, Message } from "../libs/Error";
 import { ObjectId } from "mongoose";
-<<<<<<< HEAD
-
-=======
-import MemberService from "../models/Member.service";
->>>>>>> 33c554c (fix: develop)
+import MemberService from "./Member.service";
 import { OrderStatus } from "../libs/enums/order.enum";
 
 class OrderService {
